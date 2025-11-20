36195e3e3334605aee734c8e1613c6e261d63b6c
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-20 12:36:08 +0100

    create basic html elements for title, input and button for the search bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ function App() {
 
   return (
     <>
-      
+      <header>
+        <h1>Boolflix</h1>
+        <form action="submit">
+          <input type="text" placeholder="Cerca..."/>
+          <button type="submit">Cerca</button>
+        </form>
+      </header>
+      <main></main>
     </>
   )
 }
